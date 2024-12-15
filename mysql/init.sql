921d654dee99250f279e7cd7fbcd4061d13062c4
Chris-Fotso <chrisfotso@icloud.com>
2024-12-15 15:16:21 +0100

    Add Dockerfile and initialization script for MySQL

---
+++ mysql/init.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS watchdb;
+
+USE watchdb;
+
+CREATE TABLE IF NOT EXISTS watch (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    brand VARCHAR(100),
+    model VARCHAR(255),
+    price VARCHAR(50),
+    currency VARCHAR(100),
+    stores VARCHAR(100),
+    image_url VARCHAR(255),
+    source VARCHAR(100)
+);
